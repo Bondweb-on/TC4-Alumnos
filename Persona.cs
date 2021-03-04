801205b94a6e3ef5322c78886e14456e732f8913
Sebastián Gómez Morales <sebastian61910.sg@gmail.com>
2021-03-04 13:47:24 -0700

    Introducción al encapsulamiento

---
+++ Persona.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace TC4_Alumnos
+{
+    class Persona
+    {       
+        public string apellido;
+        public string nombre;
+
+        public string NombreCompleto
+        {
+            get => this.nombre + " " + this.apellido;
+        }
+
+        private string colorFavorito;
+
+        public string ColorFavorito
+        {
+            get => "color " + this.colorFavorito;
+            set => this.colorFavorito = value;
+        }
+
+        // Método constructor
+        public Persona(string nombre, string apellido)
+        {       
+            this.apellido = apellido;
+
+            this.nombre = nombre;
+        }
+
+        public void Presentarse()
+        {
+            Console.WriteLine("Hola, mi nombre es " + this.NombreCompleto);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TC4_Alumnos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona1 = new Persona("Jonathan", "Hernandéz");
+            Console.WriteLine(persona1.NombreCompleto);
+            persona1.Presentarse();
+
+            persona1.apellido = "Rodríguez";
+            Console.WriteLine(persona1.NombreCompleto);
+            persona1.Presentarse();
+
+            persona1.ColorFavorito = "Rojo";
+
+            Console.WriteLine(persona1.ColorFavorito);
+           
+        }
+    }
+}
